041f2cafc6d1aca775580ff90a2034662ea0b87a
btwitsahsan <ahsan899289@gmail.com>
2024-05-28 03:47:37 +0500

    addcategory

--- server.ts
+++ server.ts
@@ -10,9 +10,7 @@ import { admin_login, create_category } from './src/requests/admin_requests';
 const app = express();
 const PORT = process.env.PORT || 3000;
 
-app.use(cors({
-  origin: 'https://yaad-e-madina-admin.web.app'
-}));
+app.use(cors());
 app.use(express.json());
 app.use(bodyParser.urlencoded({ limit: `50mb`, extended: true }));
 
@@ -27,7 +25,7 @@ app.post("/api/owner/createAdmin", authticate_owner, create_admin);
 app.post("/api/admin/login", admin_login);
 
 // Add Category
-app.post("/api/createCategory",authticate_admin ,create_category);
+// app.post("/api/createCategory",authticate_admin ,create_category);
 
 
 
