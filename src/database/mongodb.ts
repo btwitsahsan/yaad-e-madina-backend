d5a6ac365a60691c394429a6d5d95084d86b61ea
btwitsahsan <ahsan899289@gmail.com>
2024-10-20 15:46:20 +0500

    first commit

--- src/database/mongodb.ts
+++ src/database/mongodb.ts
@@ -5,7 +5,7 @@ import mongoose from "mongoose";
 const USER = process.env.MONGO_DB_USER_NAME;
 const PASSWORD = process.env.MONGO_DB_PASSWORD;
 
-const CONN_STRING = `mongodb+srv://${USER}:${PASSWORD}@cluster0.mpt1xnt.mongodb.net/Yaad-E-Madina`;
+const CONN_STRING = `mongodb+srv://${USER}:${PASSWORD}@cluster0.ekur4.mongodb.net/Yaad-E-Madina`;
 
 export const Connect_DB = async () => {
   try {
